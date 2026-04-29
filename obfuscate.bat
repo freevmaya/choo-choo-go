90d934437f30ac27cbe43f939e65b807a6668171
Vadim Frolov <@vmaya>
2026-04-29 08:57:42 +0500

    requlary

--- obfuscate.bat
+++ obfuscate.bat
@@ -1,7 +1,11 @@
 node obfuscate.js
-xcopy ".\public\index.html" ".\prepare\mini\index.html" /Y
-xcopy ".\public\index-vk.html" ".\prepare\mini\index-vk.html" /Y
-xcopy ".\public\scripts\languages" ".\prepare\mini\scripts\languages" /E /I /Y /H /R
+REM xcopy ".\public\index.html" ".\prepare\mini\index.html" /Y
+REM xcopy ".\public\index-vk.html" ".\prepare\mini\index-vk.html" /Y
+
+xcopy ".\public\scripts\core\VKStateManager.js" ".\prepare\mini\scripts\core\VKStateManager.js" /Y
+
+xcopy ".\public\scripts\languages" ".\prepare\mini\scripts\languages" /Y
+
 xcopy ".\public\images" ".\prepare\mini\images" /E /I /Y /H /R
 xcopy ".\public\styles" ".\prepare\mini\styles" /E /I /Y /H /R
 xcopy ".\public\sounds" ".\prepare\mini\sounds" /E /I /Y /H /R
