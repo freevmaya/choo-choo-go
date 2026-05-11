92fee1031d94ea23ef335f24d4ddfe81d647bcbf
Vadim Frolov <@vmaya>
2026-05-11 14:47:53 +0500

    requlary

--- public/scripts/core/VKUser.js
+++ public/scripts/core/VKUser.js
@@ -108,7 +108,7 @@ class VKUser {
 			})
 			.then((data) => { 
 				this.haveAdv = data.result;
-				//this.showAd(); на время модерации
+				this.showAd(); на время модерации
 		  	})
 		  	.catch((error) => { 
 		  		tracer.log(error);
